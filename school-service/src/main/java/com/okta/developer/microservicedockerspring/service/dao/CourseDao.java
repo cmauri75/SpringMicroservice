0fdc46d690b9530f0f65dc7840d4d0e19feccd26
Matt Raible <matt.raible@okta.com>
2019-02-28 08:54:17 -0700

    Upgraded start branch to latest releases

--- school-service/src/main/java/com/okta/developer/microservicedockerspring/service/dao/CourseDao.java
+++
@@ -1,7 +0,0 @@
-package com.okta.developer.microservicedockerspring.service.dao;
-
-import com.okta.developer.microservicedockerspring.service.entities.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseDao extends JpaRepository<Course, Long> {
-}
--- school-service/src/main/java/com/okta/developer/microservicedockerspring/service/dao/TeachingClassDao.java
+++
@@ -1,6 +0,0 @@
-package com.okta.developer.microservicedockerspring.service.dao;
-
-import com.okta.developer.microservicedockerspring.service.entities.TeachingClass;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeachingClassDao extends JpaRepository<TeachingClass, Long> {}
