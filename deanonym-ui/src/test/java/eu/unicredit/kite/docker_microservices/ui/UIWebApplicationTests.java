492972c089ea10b025ad3d9ae90cd2668798a51a
Cesare Mauri <cmauri@gmail.com>
2021-08-19 15:55:44 +0200

    docker composization

--- deanonym-ui/src/test/java/eu/unicredit/kite/docker_microservices/ui/UIWebApplicationTests.java
+++ deanonym-ui/src/test/java/eu/unicredit/kite/docker_microservices/ui/UIWebApplicationTests.java
@@ -3,10 +3,12 @@ package eu.unicredit.kite.docker_microservices.ui;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(locations="classpath:test.properties")
 public class UIWebApplicationTests {
 
 	@Test
---
+++ docker-composeUp.sh
@@ -0,0 +1,2 @@
+docker-compose -f docker-compose-env.yml -f docker-compose-app.yml up 
+
